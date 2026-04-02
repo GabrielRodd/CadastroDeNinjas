07a26be65211e3753da3d65ac0fa088ba7fe3ee1
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-04-02 12:28:00 -0300

    Criada uma entidade no banco de dados a partir do NinjaModel

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,63 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// Entity transforma uma classe em uma entidade do DB
+@Entity
+@Table(name = "tb_cadastro") //Nome da tabela
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //Para funcionar de identificador na tabela
+    private String nome;
+    private int idade;
+    private String email;
+    private String cla;
+
+    //NoArgs Constructor
+    public NinjaModel() {
+    }
+
+    //AllArgs Constructor
+    public NinjaModel(String nome, int idade, String email, String cla) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.cla = cla;
+    }
+
+    //Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCla() {
+        return cla;
+    }
+
+    public void setCla(String cla) {
+        this.cla = cla;
+    }
+
+}
