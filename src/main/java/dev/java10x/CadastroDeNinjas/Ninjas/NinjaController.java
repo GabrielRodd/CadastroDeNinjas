e370f615316c77a2eba9b42c7ab7817db597fdbc
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-04-17 10:48:51 -0300

    feat: Adiciona os ENDPOINTS do NinjaController. Atualizando com o mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,4 +10,36 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
+
+    //Adicionar Ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado.";
+    }
+
+    //Mostrar todos os Ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas() {
+        return "Mostrando Todos Ninjas";
+    }
+
+    //Procurar Ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorId() {
+        return "Mostrando por ID";
+    }
+
+    //Alterar dados do Ninja (UPDATE)
+    @PutMapping("/editarNinjaID")
+    public String editarNinjaPorId() {
+        return "Editando Ninja por ID";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarNinjaID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por ID";
+    }
+
+
 }
