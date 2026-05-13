a211f1d93e13faf2d219f51c4818ddaa49db418d
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-05-13 09:32:40 -0300

    refactor: Inclui annotations para documentacao das rotas da API, no MissaoController e altera uma mensagem de resposta da rota delete

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,5 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +25,7 @@ public class MissaoController {
 
     //GET -- Mostrar missoes
     @GetMapping("mostrar")
+    @Operation(summary = "Mostra todas as missoes", description = "Rota mostra todas as missoes do banco de dados")
     public ResponseEntity<List<MissaoDTO>> mostrarMissoes() {
         List<MissaoDTO> listaMissoes = missaoService.mostrarMissoes();
         return ResponseEntity.status(HttpStatus.OK)
@@ -29,6 +34,11 @@ public class MissaoController {
 
     //GET -- Mostrar Missoes por ID
     @GetMapping("mostrar/{id}")
+    @Operation(summary = "Mostra uma missao", description = "Rota mostra uma missao a partir do ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missao Encontrada com Sucesso"),
+            @ApiResponse(responseCode = "404", description = "Missao nao localizada")
+    })
     public ResponseEntity<Object> mostrarPorID(@PathVariable Long id) {
         MissaoDTO missaoBuscada = missaoService.mostrarPorID(id);
         if (missaoBuscada != null) {
@@ -42,6 +52,10 @@ public class MissaoController {
 
     //POST -- Criar Missao
     @PostMapping("/criar")
+    @Operation(summary = "Criar uma missao", description = "Rota cria uma nova missao")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Missao Criada com Sucesso"),
+    })
     public ResponseEntity<String> criarMissao(@RequestBody MissaoDTO missao) {
         MissaoDTO missaoCriada = missaoService.criarMissao(missao);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -50,11 +64,18 @@ public class MissaoController {
 
     //DELETE -- Deletar Missao
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<String> deletarMissao(@PathVariable Long id) {
+    @Operation(summary = "Deletar uma missao", description = "Rota deleta uma missao pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missao Deletada com Sucesso"),
+            @ApiResponse(responseCode = "404", description = "Missao nao encontrada"),
+    })
+    public ResponseEntity<String> deletarMissao(
+            @Parameter(description = "Usuario manda o ID no caminho da requisicao")
+            @PathVariable Long id) {
         MissaoDTO missaoDeletar = missaoService.deletarMissao(id);
         if (missaoDeletar != null) {
             return ResponseEntity.status(HttpStatus.OK)
-                    .body("Missao de id " + missaoDeletar.getId() + " deletada com sucesso");
+                    .body("Missao '" + missaoDeletar.getNome() + "' deletada com sucesso. ID: " + missaoDeletar.getId());
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Missao nao encontrada no banco de dados");
@@ -63,7 +84,16 @@ public class MissaoController {
 
     //PUT -- Atualizar Missao
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<String> atualizarMissao(@PathVariable Long id, @RequestBody MissaoDTO missaoAtualizada) {
+    @Operation(summary = "Atualizar uma missao", description = "Rota atualiza uma missao pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missao Atualizada com Sucesso"),
+            @ApiResponse(responseCode = "404", description = "Missao nao encontrada"),
+    })
+    public ResponseEntity<String> atualizarMissao(
+            @Parameter(description = "Usuario manda o ID no caminho da requisicao")
+            @PathVariable Long id,
+            @Parameter(description = "Usuario manda o novo conteudo no corpo da requisicao")
+            @RequestBody MissaoDTO missaoAtualizada) {
         MissaoDTO missaoNova = missaoService.atualizaMissao(id, missaoAtualizada);
         if (missaoNova != null) {
             return ResponseEntity.status(HttpStatus.OK)
